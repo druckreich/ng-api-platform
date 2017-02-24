e55ce5ad366aa7dce7713e40d26a10bf507c6d8e
Stefan Martinek <stefan.martinek@boku.ac.at>
2017-02-24 07:32:24 +0100

    added transpilation scripts

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { HydraView } from './hydra/hydra.view';
-export { HydraCollection } from './hydra/hydra.collection';
-export { HydraMember } from './src/hydra.member';
--- index.ts
+++
@@ -1,3 +0,0 @@
-export { HydraView } from './src/hydra/hydra.view';
-export { HydraCollection } from './src/hydra/hydra.collection';
-export { HydraMember } from './src/hydra/hydra.member';
