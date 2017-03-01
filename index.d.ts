5d4481393e61f4e2943f098e2312a4b0fda5543b
Stefan Martinek <stefan.martinek@boku.ac.at>
2017-03-01 12:08:12 +0100

    added hydra error

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export { HydraView } from './hydra/hydra.view';
 export { HydraCollection } from './hydra/hydra.collection';
 export { HydraMember } from './src/hydra.member';
+export { HydraError } from './src/hydra/hydra.error';
---
+++ src/hydra/hydra.error.ts
@@ -0,0 +1,16 @@
+import { TypedJSON, JsonMember, JsonObject } from 'typedjson-npm/src/typed-json';
+import { HydraMember } from './hydra.member';
+
+@JsonObject
+export class HydraError extends HydraMember {
+
+    @JsonMember({ name: "hydra:title" })
+    title: string;
+
+    @JsonMember({ name: "hydra:description" })
+    description: string;
+
+    @JsonMember({ elements: String })
+    trace:string[];
+
+}
