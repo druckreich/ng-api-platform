89b6a23e2cb74bde18e2320bd8b98324d1ddac16
Stefan Martinek <stefan.martinek@boku.ac.at>
2017-05-18 08:06:16 +0200

    removed files used for npm publish

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { HydraView } from './src/hydra.view';
-export { HydraCollection } from './src/hydra.collection';
-export { HydraMember } from './src/hydra.member';
-export { HydraError } from './src/hydra.error';
